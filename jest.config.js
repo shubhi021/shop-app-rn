bfa9fdb16e5e4712ec4ed0c31cfdb21441513ad7
shubhi021 <srivastavashubhi559@gmail.com>
2026-08-06 15:36:57 +0530

    fix(ci): temporarily remove jest tests until Week 2 setup

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,10 @@
 module.exports = {
   preset: 'react-native',
+  transformIgnorePatterns: [
+    'node_modules/(?!(jest-)?react-native|@react-native(-community)?|react-redux|@react-navigation|firebase|@firebase|react-native-safe-area-context|react-native-gesture-handler|react-native-screens|react-native-reanimated|react-native-vector-icons)/',
+  ],
+  setupFiles: ['<rootDir>/jest.setup.js'],
+  moduleNameMapper: {
+    '^@firebase/(.*)': '<rootDir>/node_modules/@firebase/$1/dist/index.cjs.js',
+  },
 };
